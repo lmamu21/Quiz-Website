99150cfef3f88b31564a0caa63577534c62404dd
lmamu21 <lmamu21@freeuni.edu.ge>
2024-06-14 02:13:50 +0400

    database will be determined by caller. and table structure is more refined using PRIMARY, UNIQUE, NOT NULL and AUTO_INCREMENT attributes

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -1,10 +1,10 @@
-USE quiz;
-
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    user_id int,
-    username varchar(255),
-    password_hashed varchar(40)
+    user_id int AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    password_hashed varchar(40) NOT NULL,
+    PRIMARY KEY (user_id),
+    UNIQUE (username)
 )
 
