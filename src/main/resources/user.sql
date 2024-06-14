55197d94aad668aa4107489eaecaf76af2b889ec
lmamu21 <lmamu21@freeuni.edu.ge>
2024-06-14 15:03:28 +0400

    added script that creates table which handles friends mapping

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -6,5 +6,16 @@ CREATE TABLE users (
     password_hashed varchar(40) NOT NULL,
     PRIMARY KEY (user_id),
     UNIQUE (username)
-)
+);
 
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE friends (
+    user_id INT NOT NULL,
+    friend_id INT NOT NULL,
+    friendship_status varchar(50) NOT NULL,
+    PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CHECK (user_id != friend_id)
+)
