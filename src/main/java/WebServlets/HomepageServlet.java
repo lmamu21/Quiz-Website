de10e7127070f58d825ed08fd5f89e1c566925a8
gveli21 <gveli21@freeuni.edu.ge>
2024-06-22 03:54:30 +0400

    commiting added logout button

--- src/main/java/WebServlets/HomepageServlet.java
+++ src/main/java/WebServlets/HomepageServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
@@ -20,6 +21,11 @@ public class HomepageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession sess = request.getSession();
+        if(sess.getAttribute("username") == null){
+            response.sendRedirect("/Quiz-Web/Login");
+            return;
+        }
         RequestDispatcher dispatcher = request.getRequestDispatcher("/homepage/homepage.jsp");
         dispatcher.forward(request, response);
     }
