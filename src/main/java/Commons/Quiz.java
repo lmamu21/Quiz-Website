6111c28a7a4630c9cdee0b0ec9e3a042afc35a6f
Nino Nozadze <nino@Nino.local>
2024-06-23 19:43:45 +0400

    question, quiz, answer classes

--- src/main/java/Commons/Quiz.java
+++ src/main/java/Commons/Quiz.java
@@ -1,2 +1,60 @@
-package Commons;public class Quiz {
+package Commons;
+
+import java.util.ArrayList;
+
+public class Quiz {
+    private int quizID;
+    private ArrayList<QuizOptions> quizOptions;
+    private String quizName;
+    //    The text description of the quiz.
+    private String quizDescription;
+    //    The creator of the quiz (hot linked to the creator’s user page).
+    private User creator;
+
+    private ArrayList<Question> questions;
+
+    //    Quiz Options
+//    Random Questions — Allow the creator to set the quiz to either randomize the order of the questions or to
+//    always present them in the same order.
+//    One Page vs. Multiple Pages — Allow the quiz writer to determine if all the questions should appear on a single
+//    webpage, with a single submit button, or if the quiz should display a single question allow the user
+//    to submit the answer, then display another question.
+//    Immediate Correction — For multiple page quizzes, this setting determines whether the user will receive immediate
+//    feedback on an answer, or if the quiz will only be graded once all the questions have been seen and responded to.
+    private enum QuizOptions {
+        RANDOM_QUESTIONS,
+        ONE_PAGE,
+        MULTIPLE_PAGES,
+        IMMEDIATE_CORRECTION
+    }
+
+    public Quiz(int quizID, ArrayList<QuizOptions> quizOptions, String quizName, String quizDescription, User creator,
+                ArrayList<Question> questions) {
+        this.quizID = quizID;
+        this.quizOptions = quizOptions;
+        this.quizName = quizName;
+        this.quizDescription = quizDescription;
+        this.questions = questions;
+        this.creator = creator;
+    }
+
+    public int getQuizID() {
+        return quizID;
+    }
+    public ArrayList<QuizOptions> getQuizOptions() {
+        return quizOptions;
+    }
+    public String getQuizName() {
+        return quizName;
+    }
+    public String getQuizDescription() {
+        return quizDescription;
+    }
+    public User creator() {
+        return creator;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
 }
