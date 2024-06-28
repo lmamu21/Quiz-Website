5bb366c51bdb12c4c1e77664f4b24c643e429f78
lmamu21 <lmamu21@freeuni.edu.ge>
2024-06-28 12:48:16 +0400

    implemented adding quiz to database. made minor changes to Quiz and Question classes (make enum-s package protected instead of private)

--- src/main/java/Commons/DBConnection.java
+++ src/main/java/Commons/DBConnection.java
@@ -11,6 +11,7 @@ public class DBConnection {
     private String baseURL = "jdbc:mysql://localhost:3306/";
     private String user = "root";
     private String password = "admin";
+
     public DBConnection(String database) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -18,7 +19,7 @@ public class DBConnection {
             stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             stmt.execute(String.format("USE %s", database));
         } catch (Exception e) {
-            System.out.println("ERROR: Could not connect to database "+database);
+            System.out.println("ERROR: Could not connect to database " + database);
             e.printStackTrace();
         }
     }
@@ -29,22 +30,22 @@ public class DBConnection {
      * @param salt
      * @return boolean value - whether entry was inserted into the database
      */
-    public boolean addUser(String username, String password_hash, String salt){
+    public boolean addUser(String username, String password_hash, String salt) {
         int result = 0;
         StringBuilder builder = new StringBuilder();
         builder.append("INSERT INTO users");
         builder.append(" (username, password_hashed, salt) ");
         builder.append(" VALUES ");
         builder.append(String.format("(\'%s\', \'%s\', \'%s\')", username, password_hash, salt));
-        try{
+        try {
             String query = builder.toString();
             result = stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
-            if (result==0){
+            if (result == 0) {
                 throw new IllegalArgumentException();
             }
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("ERROR: Could not add user \'" + username + "\' to the database");
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("ERROR: SQL connection error");
             e.printStackTrace();
         }
@@ -52,7 +53,6 @@ public class DBConnection {
     }
 
     /**
-     *
      * @param username - username of the user
      * @return hash value of password
      */
@@ -62,16 +62,16 @@ public class DBConnection {
         try {
             this.resultSet = stmt.executeQuery(query);
 
-            if(this.resultSet.next()){
+            if (this.resultSet.next()) {
                 // means that there was entry with provided username
                 result = resultSet.getString(1);
-            }else {
+            } else {
                 throw new IllegalArgumentException();
             }
-        }catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println("ERROR: Could not find entry with username \'" + username + "\'");
-           e.printStackTrace();
-        }catch(Exception e){
+            e.printStackTrace();
+        } catch (Exception e) {
             System.out.println("ERROR: SQL connection error");
             e.printStackTrace();
         }
@@ -84,18 +84,18 @@ public class DBConnection {
      * @param salt
      * @return boolean - whether entry was updated
      */
-    public boolean changePassword(String username, String newPassword, String salt){
+    public boolean changePassword(String username, String newPassword, String salt) {
         String query = String.format("UPDATE users SET password_hashed = \'%s\', salt = \'%s\' WHERE username = \'%s\'", newPassword, salt, username);
-        try{
+        try {
             int result = stmt.executeUpdate(query);
-            if(result==0){
+            if (result == 0) {
                 throw new IllegalArgumentException();
             }
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("ERROR: Could not change password of user \'" + username + "\' to the database");
             e.printStackTrace();
             return false;
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("ERROR: SQL connection error");
             e.printStackTrace();
             return false;
@@ -104,71 +104,68 @@ public class DBConnection {
     }
 
     /**
-     *
      * @return - number of rows
      */
-    public int getCount(){
+    public int getCount() {
         int result = 0;
-        try{
+        try {
             this.resultSet = stmt.executeQuery("SELECT * FROM users");
             this.resultSet.last();
             result = this.resultSet.getRow();
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return result;
     }
 
     /**
-     *
      * @param sender
      * @param receiver
      */
-    public boolean sendFriendRequest(String sender, String receiver){
+    public boolean sendFriendRequest(String sender, String receiver) {
         String query = String.format("INSERT INTO friends VALUES (\'%s\', \'%s\', \'%s\')", getID(sender), getID(receiver), "pending");
         int result = 0;
         boolean senderExists = accountExists(sender);
         boolean receiverExists = accountExists(receiver);
-        if(senderExists && receiverExists) {
+        if (senderExists && receiverExists) {
             try {
                 result = stmt.executeUpdate(query);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }
-        else{
+        } else {
             System.out.println("One of the provided accounts does not exists");
         }
         return result == 1;
     }
 
-    public boolean acceptFriendRequest(String accepter, String requester){
+    public boolean acceptFriendRequest(String accepter, String requester) {
         boolean requestExists = false;
         String query = String.format("SELECT * FROM friends WHERE user_id = \'%s\' AND friend_id = \'%s\' AND friendship_status = \'pending\'", getID(requester), getID(accepter));
         try {
             resultSet = stmt.executeQuery(query);
             requestExists = resultSet.next();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        if(!requestExists){
+        if (!requestExists) {
             return false;
         }
 
         String updateQuery = String.format("UPDATE friends SET friendship_status = \'friends\' WHERE user_id = \'%s\' and friend_id = \'%s\'", getID(requester), getID(accepter), "friends");
-        try{
+        try {
             int rowsAffected = stmt.executeUpdate(updateQuery);
-            if (rowsAffected == 0){
+            if (rowsAffected == 0) {
                 return false;
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         updateQuery = String.format("INSERT INTO friends VALUES (\'%s\', \'%s\', \'%s\')", getID(accepter), getID(requester), "friends");
-        try{
+        try {
             int rowsAffected = stmt.executeUpdate(updateQuery);
-            if (rowsAffected == 0){
+            if (rowsAffected == 0) {
                 return false;
             }
         } catch (SQLException e) {
@@ -177,11 +174,11 @@ public class DBConnection {
         return true;
     }
 
-    private boolean accountExists(String username){
+    private boolean accountExists(String username) {
         String query = String.format("SELECT * FROM users WHERE username = \'%s\'", username);
-        try{
+        try {
             resultSet = stmt.executeQuery(query);
-            if (resultSet.next()){
+            if (resultSet.next()) {
                 return true;
             }
         } catch (SQLException e) {
@@ -190,27 +187,23 @@ public class DBConnection {
         return false;
     }
 
-    private String getID(String username){
+    private String getID(String username) {
         String query = String.format("SELECT user_id FROM users WHERE username = \'%s\'", username);
         String result = "";
-        try{
+        try {
             resultSet = stmt.executeQuery(query);
             resultSet.next();
             result = String.valueOf(resultSet.getInt("user_id"));
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return result;
     }
 
     public static void main(String[] args) {
-        DBConnection conn = new DBConnection("quiz_test");
-        conn.acceptFriendRequest("gio", "nino" );
-        conn.acceptFriendRequest( "andria", "luka");
     }
 
     /**
-     *
      * @param username username of
      * @return
      */
@@ -220,20 +213,109 @@ public class DBConnection {
         try {
             this.resultSet = stmt.executeQuery(query);
 
-            if(this.resultSet.next()){
+            if (this.resultSet.next()) {
                 // means that there was entry with provided username
                 result = resultSet.getString(1);
-            }else {
+            } else {
                 throw new IllegalArgumentException();
             }
-        }catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println("ERROR: Could not find entry with username \'" + username + "\'");
             e.printStackTrace();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("ERROR: SQL connection error");
             e.printStackTrace();
         }
-       return result;
+        return result;
     }
+
+
+    public boolean createQuiz(Quiz quiz) {
+        String insertSQL = "INSERT INTO quizzes (quiz_name, quiz_description, author_id, random_questions_option, page_options, immediate_correction) VALUES (?,?,?,?,?,?)";
+        int quiz_id = 0;
+        try{
+            PreparedStatement preparedStatement = con.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
+
+
+            preparedStatement.setString(1, quiz.getQuizName());
+            preparedStatement.setString(2, quiz.getQuizDescription());
+            preparedStatement.setString(3, getID(quiz.creator().getUsername()));
+            preparedStatement.setBoolean(4, quiz.getQuizOptions().contains(Quiz.QuizOptions.RANDOM_QUESTIONS) ? true : false);
+            preparedStatement.setString(5, quiz.getQuizOptions().contains(Quiz.QuizOptions.MULTIPLE_PAGES) ? "multiple-page" : "one-page");
+            preparedStatement.setBoolean(6, quiz.getQuizOptions().contains(Quiz.QuizOptions.IMMEDIATE_CORRECTION) ? true : false);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                try(ResultSet rs = preparedStatement.getGeneratedKeys()){
+                    if(rs.next()) {
+                        quiz_id = rs.getInt(1);
+                    }
+                }
+            }
+            else {
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+
+        // if reached here, entry in quizzes is inserted, but questions are not populated
+        for(Question question : quiz.getQuestions()) {
+            insertSQL = "INSERT INTO questions (question_text, question_num, quiz_id, image_url, multiple_choice) VALUES (?, ?, ?, ?, ?)";
+            int question_id = 0;
+            try{
+                PreparedStatement preparedStatement = con.prepareStatement(insertSQL,  Statement.RETURN_GENERATED_KEYS);
+
+                preparedStatement.setString(1, question.getQuestion());
+                preparedStatement.setInt(2, question.getQuestionIndex());
+                preparedStatement.setInt(3, quiz_id);
+                if(question.getUrl() == null || question.getUrl().isEmpty()){
+                    preparedStatement.setNull(4, Types.VARCHAR);
+                }else{
+                    preparedStatement.setString(4, question.getUrl());
+                }
+                preparedStatement.setBoolean(5, question.getQuestionType() == Question.QuestionType.MULTIPLE_CHOICE);
+                int rowsAffected = preparedStatement.executeUpdate();
+                if(rowsAffected > 0) {
+                    try(ResultSet rs = preparedStatement.getGeneratedKeys()) {
+                        if(rs.next()){
+                            question_id = rs.getInt(1);
+                        }
+                    }
+                }else{
+                    return false;
+                }
+
+
+
+            }catch (SQLException e){
+                e.printStackTrace();
+                return false;
+            }
+            if(question.getQuestionType() == Question.QuestionType.MULTIPLE_CHOICE){
+                insertSQL = "INSERT INTO multiple_choice_answers (option_char, answer_text, question_id, correct_answer) VALUES (?, ?, ?, ?)";
+                char option_char = 'A';
+                for(Answer answer : question.getAnswers()) {
+                    try{
+                        PreparedStatement preparedStatement = con.prepareStatement(insertSQL);
+                        preparedStatement.setString(1, String.valueOf(option_char));
+                        option_char++;
+                        preparedStatement.setString(2, answer.getAnswer());
+                        preparedStatement.setInt(3, question_id);
+                        preparedStatement.setBoolean(4, answer.isCorrect());
+                    }
+                    catch (SQLException e){
+                        e.printStackTrace();
+                        return false;
+                    }
+                }
+
+            }
+        }
+
+
+        return true;
+    }
+
 }
 
