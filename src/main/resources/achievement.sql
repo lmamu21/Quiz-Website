2ac28783298815feaeb73ce439959ceca7b1a2ac
Nino Nozadze <ninonozadze07@gmail.com>
2024-07-09 19:30:07 +0400

    minor changes

--- src/main/resources/achievement.sql
+++ src/main/resources/achievement.sql
@@ -2,6 +2,5 @@ CREATE TABLE achievement (
     achievement_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     achievement_type INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
