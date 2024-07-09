41e35c16710ee7c0c2a61d8f51d5f89c63be2181
Nino Nozadze <ninonozadze07@gmail.com>
2024-07-09 19:35:17 +0400

    announcement  DAO and Manager file configuration

--- src/main/java/Commons/Announcement/AnnouncementDAO.java
+++ src/main/java/Commons/Announcement/AnnouncementDAO.java
@@ -1,4 +1,83 @@
 package Commons.Announcement;
 
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AnnouncementDAO {
+    private DataSource pool;
+    private String databaseName;
+
+    public AnnouncementDAO(DataSource pool, String databaseName) {
+        this.pool = pool;
+        this.databaseName = databaseName;
+    }
+
+    public void addAnnouncement(String announcement) throws SQLException {
+        Connection con = null;
+        String query = String.format("INSERT INTO announcement (announcement) VALUES (\'%s\')", announcement);
+        con = pool.getConnection();
+        Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+    }
+
+    public ArrayList<Announcement> getAnnouncements() throws SQLException {
+        ArrayList<Announcement> announcements = new ArrayList<Announcement>();
+
+        Connection con = null;
+        try {
+            con = pool.getConnection();
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            String query = "SELECT * FROM announcement";
+            ResultSet resultSet = stmt.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("announcementID");
+                String announcementText = resultSet.getString("announcement");
+                announcements.add(new Announcement(id, announcementText));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
+
+        return announcements;
+    }
+
+    public ArrayList<Announcement> getLastAnnouncements() throws SQLException {
+        ArrayList<Announcement> announcements = new ArrayList<Announcement>();
+
+        Connection con = null;
+        try {
+            con = pool.getConnection();
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            String query = "SELECT * FROM announcement ORDER BY announcementID DESC LIMIT 5";
+            ResultSet resultSet = stmt.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("announcementID");
+                String announcementText = resultSet.getString("announcement");
+                announcements.add(new Announcement(id, announcementText));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
+
+        return announcements;
+    }
+
+
 }
--- src/main/java/Commons/Announcement/AnnouncementManager.java
+++ src/main/java/Commons/Announcement/AnnouncementManager.java
@@ -1,4 +1,26 @@
 package Commons.Announcement;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class AnnouncementManager {
+
+    private AnnouncementDAO annDAO;
+
+    public AnnouncementManager(AnnouncementDAO dao) {
+        annDAO = dao;
+    }
+
+    public void addAnnouncement(String announcement) throws SQLException {
+        annDAO.addAnnouncement(announcement);
+    }
+
+
+    public ArrayList<Announcement> getAnnouncements(int userId) throws SQLException {
+        return annDAO.getAnnouncements();
+    }
+
+    public ArrayList<Announcement> getLastAnnouncements() throws SQLException {
+        return annDAO.getLastAnnouncements();
+    }
 }
